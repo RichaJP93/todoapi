cd22c892ff03d032013abc1a7ce7fb3726efbc6f
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-25 11:56:20 +0000

    added routes for main page + specific page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Todos from './components/Todos'
+import Todo from './components/Todo'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/" children={<Todos />}></Route>
+          <Route exact path="/todo/:id" children={<Todo />}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
