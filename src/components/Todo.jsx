f5138b3002133e7e34de7a36fee5c89b7c59bef4
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-25 14:23:40 +0000

    display the details for a todo when clicked on main list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import { CircularProgress } from '@material-ui/core';
 
 const Todo = () => {
   const { id } = useParams();
+
+  const [todoDetails, setTodoDetails] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then((res) => {
+      const response = res.data;
+      setTodoDetails(response);
+    });
+  }, []);
+
+  //destructring response object from state, 
+  //renaming id from response to not conflict with id from params
+  const { id: todoId, userId, title, completed } = todoDetails;
+  console.log(todoDetails)
   return (
-    <p>{`This is the TODO component, the TODO id is ${id}`} </p>
-  )
-}
+    <div>
+      {todoDetails ? (
+        <div>
+          <h1>{`Todo id: ${todoId}`}</h1>
+          <h1>{`Todo user ID: ${userId}`}</h1>
+          <h1>{`Title: ${title}`}</h1>
+          <h1>{`Completed: ${completed}`}</h1>
+        </div>
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
+};
 
 export default Todo;
